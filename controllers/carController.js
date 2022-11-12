33b0e3adc5a8032eab76a688aada2b96b13f9310
Juan Cruz Romero Garcia <rgjuancruz@gmail.com>
2022-11-12 20:25:06 -0300

    Get all cars

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,10 +1,26 @@
-const Car = require('../models/mongo/Car');
-const bcryptjs = require('bcryptjs');
-const { validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
+const Car = require("../models/mongo/Car");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
-exports.newCar = async (req, res) => {
+/**
+ * Get all cars
+ */
+const getCars = async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.status(200).json(cars);
+  } catch (error) {
+    res.status(400).json(error).send("Sorry, something went wrong");
+  }
+};
+
+/**
+ * Add new car
+ */
+const newCar = async (req, res) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ msg: errors.array() });
   }
@@ -13,13 +29,20 @@ exports.newCar = async (req, res) => {
   try {
     let car = await Car.findOne({ licensePlate });
     if (car) {
-      return res.status(400).json({ msg: 'An car already exist with this license plate' });
+      return res
+        .status(400)
+        .json({ msg: "An car already exist with this license plate" });
     }
     car = new Car(req.body);
     await car.save();
-    res.json({ msg: 'Car created successfuly', car: car });
+    res.json({ msg: "Car created successfuly", car: car });
   } catch (error) {
     console.log(error);
-    res.status(400).send('Sorry, something went wrong');
+    res.status(400).send("Sorry, something went wrong");
   }
+};
+
+module.exports = {
+  getCars,
+  newCar,
 };
--- routes/cars.js
+++ routes/cars.js
@@ -1,23 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const carController = require('../controllers/carController');
-const { check } = require('express-validator');
+const { getCars, newCar } = require("../controllers/carController");
+const { check } = require("express-validator");
+
+router.get("/", getCars);
 
 router.post(
-  '/',
+  "/",
   [
-    check('licensePlate', 'The license plate field is required').not().isEmpty(),
-    check('brand', 'The brand field is required').not().isEmpty(),
-    check('model', 'The model field is required').not().isEmpty(),
-    check('year', 'The year field is required').not().isEmpty(),
-    check('price', 'The price field is required').not().isEmpty(),
-    check('description', 'The description field is required').not().isEmpty(),
-    check('color', 'The color field is required').not().isEmpty(),
-    check('doors', 'The doors field is required').not().isEmpty(),
-    check('fuel', 'The fuel field is required').not().isEmpty(),
-    check('milage', 'The milage field is required').not().isEmpty()
+    check("licensePlate", "The license plate field is required")
+      .not()
+      .isEmpty(),
+    check("brand", "The brand field is required").not().isEmpty(),
+    check("model", "The model field is required").not().isEmpty(),
+    check("year", "The year field is required").not().isEmpty(),
+    check("price", "The price field is required").not().isEmpty(),
+    check("description", "The description field is required").not().isEmpty(),
+    check("color", "The color field is required").not().isEmpty(),
+    check("doors", "The doors field is required").not().isEmpty(),
+    check("fuel", "The fuel field is required").not().isEmpty(),
+    check("milage", "The milage field is required").not().isEmpty(),
   ],
-  carController.newCar
+  newCar
 );
 
 module.exports = router;
