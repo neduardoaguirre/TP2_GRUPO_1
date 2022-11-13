6e9a80243e802c55574ffc3b4e3dd96ce69289df
Juan Cruz Romero Garcia <rgjuancruz@gmail.com>
2022-11-12 22:18:21 -0300

    Add get cars swagger

--- routes/cars.js
+++ routes/cars.js
@@ -1,3 +1,82 @@
+// routes/cars.js
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Car:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           example: "636af9b732bdc95cd6db3592"
+ *         licensePlate:
+ *           type: string
+ *           example: "AE788EB"
+ *         brand:
+ *           type: string
+ *           example: "Toyota"
+ *         model:
+ *           type: string
+ *           example: "Yaris"
+ *         year:
+ *           type: integer
+ *           example: 2022
+ *         price:
+ *           type: integer
+ *           example: 2500000
+ *         description:
+ *           type: string
+ *           example: "Full con ABS"
+ *         image:
+ *           type: string
+ *           example: "https://www.jeep.com.ar/content/dam/cross-regional/latam/jeep/es_ar/bhp/lineup/Jeep-Renegade-2022-new.jpg.img.300.jpg"
+ *         color:
+ *           type: string
+ *           example: "Gris"
+ *         doors:
+ *           type: integer
+ *           example: 5
+ *         fuel:
+ *           type: string
+ *           example: "Nafta"
+ *         milage:
+ *           type: integer
+ *           example: 1000
+ *         __v:
+ *           type: integer
+ *           example: 0
+ *       required:
+ *         - licensePlate
+ *         - brand
+ *         - model
+ *         - year
+ *         - price
+ *         - description
+ *         - color
+ *         - doors
+ *         - fuel
+ *         - milage
+ */
+
+/**
+ * @swagger
+ *
+ * /cars:
+ *  get:
+ *    summary: Get list of all cars.
+ *    description: Get list of all cars.
+ *    responses:
+ *      200:
+ *        description: A list of cars.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Car'
+ */
+
 const express = require("express");
 const router = express.Router();
 const {
