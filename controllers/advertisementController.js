1fae82413f0e4055a57d4ac85ac0487c8fe4cb23
Camila <>
2022-11-15 21:38:09 -0300

    advertisements endpoint & swagger

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -3,7 +3,7 @@ const Car = require("../models/Car");
 // const Comment = require("../models/Comment");
 const { validationResult } = require("express-validator");
 
-exports.newAdvertisement = async (req, res) => {
+const newAdvertisement = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ msg: errors.array() });
@@ -34,3 +34,77 @@ exports.newAdvertisement = async (req, res) => {
     res.status(400).send("Sorry, something went wrong");
   }
 };
+
+/**
+ * Get advertisement by id
+ */
+
+const getAdvertisementById = async (req, res) => {
+  const { id } = req.params
+  const adv = await Advertisement.findById(id)
+
+  try {
+    if (!adv) {
+      return res.status(400).json({msg: `Advertisement with id (${id}) does not exist`})
+    } 
+    return res.status(200).json(adv)
+   
+  } catch (error) {
+    res.status(400).json(error).send("Sorry, something went wrong");
+  }
+};
+
+/**
+ * Get all advertisements
+ */
+
+ const getAllAdvertisements = async (req, res) => {
+  try {
+    const advs = await Advertisement.find();
+    res.status(200).json(advs);
+  } catch (error) {
+    res.status(400).json(error).send("Sorry, something went wrong");
+  }
+};
+
+/**
+ * Update advertisement by id
+ */
+ const updateAdvertisementById = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ msg: errors.array() });
+  }
+
+  try {
+    await Advertisement.findByIdAndUpdate({ _id: req.params.id}, { $set: req.body }, { new: true });
+    res.status(200).json("Advertisement has been updated");
+  } catch (error) {
+    res.status(404).json(error).send("There is no advertisement with this id");
+  }
+};
+
+/**
+ * Delete advertisement by id
+ */
+ const deleteAdvertisementById = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ msg: errors.array() });
+  }
+
+  try {
+    await Advertisement.findByIdAndRemove({ _id: req.params.id });
+    res.status(200).json("Advertisement has been deleted");
+  } catch (error) {
+    res.status(404).json(error).send("There is no advertisement with this id");
+  }
+};
+
+exports.module = {
+  newAdvertisement,
+  getAdvertisementById,
+  deleteAdvertisementById,
+  updateAdvertisementById,
+  getAllAdvertisements
+}
--- routes/advertisements.js
+++ routes/advertisements.js
@@ -1,17 +1,206 @@
-const express = require('express');
-const router = express.Router();
-const advertisementController = require('../controllers/advertisementController');
-const { check } = require('express-validator');
-const { isAdmin } = require('../middleware/role');
+// routes/advertisements.js
 
-router.post(
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Advertisements:
+ *       allOf:
+ *         - type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *               example: "636af9b732bdc95cd6db3592"
+ *             __v:
+ *               type: integer
+ *               example: 0
+ *         - $ref: '#/components/schemas/BodyAdvertisement'
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     BodyAdvertisement:
+ *       type: object
+ *       properties:
+ *         car: 
+ *           type: object
+ *           example: 
+ *              licensePlate: "AE788EB"
+ *              brand: "Toyota"
+ *              model: "Yaris"
+ *              year: 2022
+ *              price: 2500000
+ *              description: "Full con ABS"
+ *              image: "https://www.jeep.com.ar/content/dam/cross-regional/latam/jeep/es_ar/bhp/lineup/Jeep-Renegade-2022-new.jpg.img.300.jpg"
+ *              color: "Gris"
+ *              doors: 5
+ *              fuel: "Nafta"
+ *              milage: 1000
+ *         date:
+ *            type: date
+ *            example: "2022-11-13T18:02:25.319Z"
+ *         comments:
+ *            type: array
+ *         payed:
+ *            type: boolean
+ *            example: true
+ *       required:
+ *         - car
+ *         - date
+ */
+
+ const express = require('express');
+ const router = express.Router();
+ const { getAdvertisementById, updateAdvertisementById, deleteAdvertisementById, getAllAdvertisements, newAdvertisement } = require('../controllers/advertisementController');
+ const { check } = require('express-validator');
+ const { isAdmin } = require('../middleware/role');
+
+
+ /**
+ * @swagger
+ *
+ * /advertisements/{id}:
+ *  get:
+ *    tags:
+ *      - Advertisements
+ *    summary: Get an advertisement by id.
+ *    description: Get an advertisement by id.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        requerid: true
+ *        description: Id of the advertisement
+ *    responses:
+ *      200:
+ *        description: An advertisement.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Advertisement'
+ */
+
+router.get('/:id',
+[
+  isAdmin,   
+  check('id', 'Please enter a valid id').isMongoId(),
+], getAdvertisementById);
+
+/**
+ * @swagger
+ *
+ * /advertisements:
+ *  get:
+ *    tags:
+ *      - Advertisements
+ *    summary: Get list of all advertisements.
+ *    description: Get list of all advertisements.
+ *    responses:
+ *      200:
+ *        description: A list of advertisements.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Advertisement'
+ */
+
+ router.get('/', isAdmin, getAllAdvertisements);
+
+/**
+ * @swagger
+ *
+ * /advertisements:
+ *  post:
+ *    tags:
+ *      - Advertisements
+ *    summary: Add new advertisement.
+ *    description: Add new advertisement.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/BodyAdvertisement'
+ *    responses:
+ *      200:
+ *        description: An advertisement.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                msg:
+ *                  type: string
+ *                  example: "Advertisement created successfully"
+ *                advertisement:
+ *                  type: object
+ *                  $ref: '#/components/schemas/Advertisement'
+ */
+
+ router.post(
   '/',
   isAdmin,
   [
     check('car', 'The car field is required').not().isEmpty(),
     check('payed', 'The payed field is required').not().isEmpty()
   ],
-  advertisementController.newAdvertisement
+  newAdvertisement
 );
 
+/**
+ * @swagger
+ *
+ * /advertisements/{id}:
+ *  put:
+ *    tags:
+ *      - Advertisements
+ *    summary: Update an advertisement by id.
+ *    description: Update an advertisement by id.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        requerid: true
+ *        description: Id of the advertisement
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/BodyAdvertisement'
+ *    responses:
+ *      200:
+ *        description: An advertisement.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Advertisement'
+ */
+
+router.put('/:id', isAdmin, updateAdvertisementById);
+
+/**
+ * @swagger
+ *
+ * /advertisements/{id}:
+ *  delete:
+ *    tags:
+ *      - Advertisements
+ *    summary: Delete an advertisement by id.
+ *    description: Delete an advertisement by id.
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        requerid: true
+ *        description: Id of the advertisement
+ *    responses:
+ *      200:
+ *        description: If the operation was successfully return 200.
+ */
+
+router.delete('/:id', isAdmin, deleteAdvertisementById);
+
 module.exports = router;
+
